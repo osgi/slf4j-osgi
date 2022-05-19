a39dc5ff1f0590ef4aea42803f7536c314c22e5c
BJ Hargrave <hargrave@us.ibm.com>
2022-05-19 16:04:48 -0400

    build: Fix package version for 1.7.2
    Signed-off-by: BJ Hargrave <hargrave@us.ibm.com>

--- src/main/java/org/slf4j/impl/package-info.java
+++ src/main/java/org/slf4j/impl/package-info.java
@@ -17,7 +17,7 @@
  *******************************************************************************/
 
 @Export
-@Version("1.7.1")
+@Version("1.7.2")
 @Header(name = Constants.FRAGMENT_HOST, value = "slf4j.api;bundle-version=\"[1.7.0,1.8.0)\"")
 package org.slf4j.impl;
 
