d15059d71e9cc0bd5f70b61698595069df7fc6bf
BJ Hargrave <hargrave@us.ibm.com>
2022-08-30 19:10:46 -0400

    Update implementation for slf4j 2.0
    slf4j 2.0 changes to use the ServiceLoader model to obtain the
    implementation. So we must change. The bundle is no longer a fragment
    and uses the OSGi Service Loader Mediator spec to provide the
    implementation to the slf4j.api bundle.
    
    Since we no longer need to implement a specific class, we refactor
    into a new package.
    
    Signed-off-by: BJ Hargrave <hargrave@us.ibm.com>

--- src/main/java/org/osgi/slf4j/package-info.java
+++ src/main/java/org/osgi/slf4j/package-info.java
@@ -16,12 +16,7 @@
  * SPDX-License-Identifier: Apache-2.0
  *******************************************************************************/
 
-@Export
-@Version("1.7.2")
-@Header(name = Constants.FRAGMENT_HOST, value = "slf4j.api;bundle-version=\"[1.7.0,1.8.0)\"")
-package org.slf4j.impl;
+@Version("2.0.0")
+package org.osgi.slf4j;
 
-import org.osgi.annotation.bundle.Export;
-import org.osgi.annotation.bundle.Header;
 import org.osgi.annotation.versioning.Version;
-import org.osgi.framework.Constants;
