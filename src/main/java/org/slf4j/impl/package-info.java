925c9fce58a0f51c9c32d311099834d45fbec178
BJ Hargrave <hargrave@us.ibm.com>
2022-09-15 10:31:40 -0400

    branch-1.7: Move to 1.7.3-SNAPSHOT
    Signed-off-by: BJ Hargrave <hargrave@us.ibm.com>

--- src/main/java/org/slf4j/impl/package-info.java
+++ src/main/java/org/slf4j/impl/package-info.java
@@ -17,7 +17,7 @@
  *******************************************************************************/
 
 @Export
-@Version("1.7.2")
+@Version("1.7.3")
 @Header(name = Constants.FRAGMENT_HOST, value = "slf4j.api;bundle-version=\"[1.7.0,1.8.0)\"")
 package org.slf4j.impl;
 
